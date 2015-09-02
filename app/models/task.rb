1c87b981b9f6d80fdd666daf8a1ad9d5b488cad5
Yen-Nan Lin <yennanlin.tw@gmail.com>
2015-09-02 20:54:22 +0800

    Add epub file validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ActiveRecord::Base
   mount_uploader :ebook, EbookUploader
 
   validates :email, presence: true, email: true
+  validate :ebook_size
 
   before_destroy :destroy_mobi_file
 
@@ -10,5 +11,9 @@ class Task < ActiveRecord::Base
 
   def destroy_mobi_file
     system('rm', mobi_path) unless mobi_path.nil?
+  end
+
+  def ebook_size
+    errors.add(:ebook, "should be less than 12MB") if ebook.size > 12.megabytes
   end
 end
